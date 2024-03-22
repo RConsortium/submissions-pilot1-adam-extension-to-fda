3f1edb23d75189d5ace261750d58fffea156275d
Joel Laxamana <laxamanj@gene.com>
2024-03-22 18:51:20 +0000

    updated spec spreadsheet name

--- batch_copy.sh
+++ batch_copy.sh
@@ -5,7 +5,7 @@ cd ./m5/datasets/rconsortiumpilot3
 git clone https://github.com/RConsortium/submissions-pilot3-adam.git --branch main --single-branch ./submission-pilot3-adam --depth 1
 
 mv ./submission-pilot3-adam/submission/sdtm/*.xpt ./submission-pilot3-adam/submission/sdtm/define.* ./tabulations/sdtm
-mv ./submission-pilot3-adam/submission/adam/*.xpt ./submission-pilot3-adam/submission/adam/define* './submission-pilot3-adam/submission/adam/ADaM - Pilot 3.xlsx' ./analysis/adam/datasets
+mv ./submission-pilot3-adam/submission/adam/*.xpt ./submission-pilot3-adam/submission/adam/define* './submission-pilot3-adam/submission/adam/adam-pilot-3.xlsx' ./analysis/adam/datasets
 mv ./submission-pilot3-adam/submission/programs/ad*.r ./submission-pilot3-adam/submission/programs/tlf*.r ./analysis/adam/programs
 mv ./submission-pilot3-adam/renv.lock ./analysis/adam/programs/renv-lock.txt
 
